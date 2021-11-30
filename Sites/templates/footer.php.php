594c173091100d6d5653161e0211cb9642b69c53
CarolinaMezaCh <80854561+CarolinaMezaCh@users.noreply.github.com>
2021-11-30 19:47:31 -0300

    Add files via upload

---
+++ Sites/templates/footer.php.php
@@ -0,0 +1,9 @@
+        <!-- https://bulma.io/documentation/layout/footer/ -->
+        <footer class="footer has-background-black-ter has-text-white-ter">
+            <div class="content has-text-centered">
+                <p>IIC2413 - Ayudantía 3 BDD</p>
+            </div>
+        </footer>
+    </body>
+</html>
+© 2021 GitHub, Inc.
